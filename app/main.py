b2897d55dff0398993408c659921ef68c6d189e5
Bence Molnár <xjarekx@gmail.com>
2026-03-02 20:55:01 +0100

    Add item retrieval endpoint and corresponding test cases

--- app/main.py
+++ app/main.py
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 async def root() -> dict:
     return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int) -> dict:
+    return {"item_id": item_id}
--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,9 @@ def test_main_ok() -> None:
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+
+def test_read_item_ok() -> None:
+    response = client.get("/items/42")
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 42}
